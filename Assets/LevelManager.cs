8c4dcd9cab24dccc352d040f3f18494f009d3b39
WarrenStr <59877718+WarrenStr@users.noreply.github.com>
2025-01-14 17:56:08 -0500

    Grid and level

---
+++ Assets/LevelManager.cs
@@ -0,0 +1,40 @@
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    public int rows = 10;
+    public int columns = 10;
+    public float cellsize = 3;
+
+    public GameObject[] tiles;
+    public GameObject plane;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Destroy(plane);
+        GenerateGrid();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void GenerateGrid()
+    {
+        for (int r = 0; r < rows; r++) // r = rows 
+        {
+            for(int c = 0; c < columns; c++) // c = columns
+            {
+                Vector3 position = new Vector3(c * cellsize, 0, r * cellsize);
+                
+                GameObject tile = tiles[Random.Range(0, tiles.Length)];
+
+                Instantiate(tile, position, Quaternion.identity, transform);
+            }
+        }
+    }
+}
